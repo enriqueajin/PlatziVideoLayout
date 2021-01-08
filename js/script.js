06bdcdddc822e900ffaa38122a6dc1010f60a179
Enrique Ajin <enriquesaon@gmail.com>
2021-01-08 17:58:42 -0600

    Se finalizan los Layout para tablet y desktop.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 function showNav() {
-    var x = document.getElementById("menu");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-      
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    
+  } else {
+    x.style.display = "block";
+  }
+}
+
+const mediaQuery = window.matchMedia('(min-width: 768px)')
+
+if (mediaQuery.matches) {
+  var x = document.getElementById("menu");
+  x.style.display = "block";
 }
