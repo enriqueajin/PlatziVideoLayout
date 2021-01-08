5d0bfdafeb107a580f839e49214be03322c4e4c4
Enrique Ajin <enriquesaon@gmail.com>
2021-01-08 13:44:53 -0600

    Se terminó el Layout para móvil. Pendiente tablet y desktop.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function showNav() {
     var x = document.getElementById("menu");
     if (x.style.display === "block") {
       x.style.display = "none";
+      
     } else {
       x.style.display = "block";
     }
