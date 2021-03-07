c23c4a281ea74f96c67de6df428f712e60a298be
Enrique Ajin <enriquesaon@gmail.com>
2021-03-06 22:44:15 -0600

    Se corrigió error que hacía que se ocultara el menú de navegación cuando se usaba el menú de hamburguesa en móvil y tablet y después se pasaba al layout de desktop.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,60 @@
+//------------------------------------------------------------// Obtener elementos del DOM
+
+const menu = document.getElementById('menu')
+const header = document.getElementById('header')
+const main = document.getElementById('main')
+let desktopMedia = window.matchMedia('(min-width: 900px)')
+let mobileMedia = window.matchMedia('(max-width: 900px)')
+//------------------------------------------------------------
+
+// Función al dar clic en menú de hamburguesa (mobile y tablet
+//------------------------------------------------------------
 function showNav() {
-  var x = document.getElementById("menu");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  if (menu.style.height == "100%") {
+    menu.style.height = "0%"
+    header.style.position = "static"
+    main.style.marginTop = "0px"
     
   } else {
-    x.style.display = "block";
+    menu.style.height = "100%"
+    header.style.position = "fixed"
+    main.style.marginTop = "66.64px"
   }
 }
+//------------------------------------------------------------
 
-const mediaQuery = window.matchMedia('(min-width: 768px)')
-
-if (mediaQuery.matches) {
-  var x = document.getElementById("menu");
-  x.style.display = "block";
+// Función para validar si se está en media query Desktop
+//------------------------------------------------------------
+function matchDesktopMedia(media) {
+  if (media.matches) {
+    menu.style.height = "100%"
+  }
+  return media.matches
 }
+//------------------------------------------------------------
+
+//------------------------------------------------------------
+// Función para validar si se está en media query Mobile o Tablet
+function matchMobileMedia(media) {
+  if (media.matches) {
+    menu.style.height = "0%"
+  }
+  return media.matches
+}
+//------------------------------------------------------------
+
+//------------------------------------------------------------
+// Función que se ejecuta cuando el viewport de la pantalla cambia
+function onResize() {
+  if (matchDesktopMedia(desktopMedia)) {
+    menu.style.height = "100%"
+    menu.style.transition = "none"
+  } else {
+    menu.style.transition = "0.5s"
+    matchMobileMedia(mobileMedia)
+  }
+}
+//------------------------------------------------------------
+
+matchDesktopMedia(desktopMedia)
+window.addEventListener('resize', onResize)
